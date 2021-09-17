d00cdb9cd750cdf630d8a4d9a69daf4fb3ccf04c
Advay Patil <advaypatil27@gmail.com>
2021-09-16 20:46:41 -0700

    Add "Mean" function

---
+++ statistics.js
@@ -0,0 +1,18 @@
+const Mean = (dataSet = "") => {
+  const numbersString = dataSet.split(",");
+  const numbers = numbersString.map((element, idx) => {
+    return parseFloat(element);
+  });
+
+  console.log(numbers);
+
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+
+  return sum / numbers.length;
+};
+
+console.log(Mean("1, 2, 3, 4"));
